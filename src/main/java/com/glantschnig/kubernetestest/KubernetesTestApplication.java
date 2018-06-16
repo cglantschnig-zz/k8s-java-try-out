16977b3084a25c875380438039853edf74a4466b
Christopher Glantschnig <christopher.glantschnig@gmail.com>
2018-06-16 22:20:40 +0200

    feat: initial commit

---
+++ src/main/java/com/glantschnig/kubernetestest/KubernetesTestApplication.java
@@ -0,0 +1,12 @@
+package com.glantschnig.kubernetestest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesTestApplication.class, args);
+	}
+}
---
+++ src/main/java/com/glantschnig/kubernetestest/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.glantschnig.kubernetestest.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String index() {
+        return "Hello Kubernetes";
+    }
+
+}
