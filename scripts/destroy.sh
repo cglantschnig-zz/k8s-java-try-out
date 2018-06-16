09486851d0ac457df616517f160468f797af5cf6
Christopher Glantschnig <christopher.glantschnig@gmail.com>
2018-06-16 22:36:16 +0200

    feat: add k8s config

---
+++ scripts/destroy.sh
@@ -0,0 +1,4 @@
+kubectl delete deployment java-deployment
+kubectl delete deployment redis-deployment
+kubectl delete service java
+kubectl delete service redis
---
+++ scripts/setup.sh
@@ -0,0 +1,4 @@
+kubectl apply -f ./k8s/deployments/java.yml
+kubectl apply -f ./k8s/deployments/redis.yml
+kubectl apply -f ./k8s/services/java.yml
+kubectl apply -f ./k8s/services/redis.yml
